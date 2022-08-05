dc4c7ae9ae23b2c2d7c91b94fa0adc525301f168
GaoFeng Zhao <gaofengz@xsjapps58.xilinx.com>
2022-08-05 01:37:20 -0700

    modify plm.tcl

--- Scripts/vck190_vitis_plm.tcl
+++ Scripts/vck190_vitis_plm.tcl
@@ -1,17 +1,27 @@
 
-setws ../Software/Vitis/prj_plm
+setws -switch ../Software/Vitis/prj_plm
 
 platform create -name vck190_wrapper -hw ../Hardware/vck190_ddr4/design_1_wrapper.xsa
 
-bsp config plm_usb_en "true"
-
-platform clean
+domain create -name pmc -os standalone -proc versal_cips_0_pspmc_0_psv_pmc_0 -support-app {versal_plm}
 
 platform generate
 
-domain create -name pmc -os standalone -proc versal_cips_0_pspmc_0_psv_pmc_0
+bsp reload
+
+bsp config plm_usb_en "true"
+
+bsp write
+
+bsp reload
+
+catch {bsp regenerate}
+
+platform generate
 
 app create -name plm -sysproj plm_system -platform vck190_wrapper -domain pmc -template {versal PLM} -lang C
 	
-app build -name hello_a72_0
+app build -name plm
+
+
 
